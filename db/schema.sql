64a105773e12f585e7760f40a652b917c5556000
HenniePenny <h.kressner@hotmail.de>
2022-08-27 14:45:18 +0200

    update id in tables w/ auto-increment

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,14 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  department_id INT NOT NULL PRIMARY KEY,
+  department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-  role_id INT NOT NULL PRIMARY KEY,
+  role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL (7,2) NOT NULL,
+  salary DECIMAL (8,2) NOT NULL,
   department_id INT NOT NULL,
   FOREIGN KEY (department_id)
   REFERENCES department(department_id)
@@ -19,13 +19,13 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-  employee_id INT NOT NULL PRIMARY KEY,
+  employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   FOREIGN KEY (role_id)
   REFERENCES role(role_id)
   ON DELETE SET NULL,
-  manager_id INT 
+  manager_id INT NOT NULL
   REFERENCES employee(employee_id)
 );
