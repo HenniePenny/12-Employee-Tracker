f51e786b090f7509b7f287ad568891c49dd239e9
HenniePenny <h.kressner@hotmail.de>
2022-08-27 11:43:08 +0200

    create tables dept, role & employee

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,30 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 
 USE employee_db;
+
+CREATE TABLE department (
+  department_id INT NOT NULL PRIMARY KEY,
+  dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  role_id INT NOT NULL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL (7,2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(department_id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  employee_id INT NOT NULL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES role(role_id)
+  ON DELETE SET NULL,
+  manager_id INT 
+  REFERENCES employee(employee_id)
+);
