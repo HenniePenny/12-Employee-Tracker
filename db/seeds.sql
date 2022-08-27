04a604156a506d245b820d0c2d2e70faaf885ab9
HenniePenny <h.kressner@hotmail.de>
2022-08-28 00:13:42 +0200

    changed seeds file, add question.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,14 +5,14 @@ VALUES ("Human Resources"),
        ("Sales");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("HR Director", 110000, 1),
-       ("HR Manager", 70000, 1),
-       ("Director Finance", 120000, 2),
-       ("Accountant", 85000, 2),
-       ("Director Legal", 150000, 3),
-       ("Lawyer", 100000, 3),
-       ("Sales Lead", 100000, 4),
-       ("Salesperson", 75000, 4);
+VALUES ("HR Director", 80000, 1),
+       ("HR Manager", 55000, 1),
+       ("Director Finance", 90000, 2),
+       ("Accountant", 60000, 2),
+       ("Director Legal", 100000, 3),
+       ("Lawyer", 80000, 3),
+       ("Sales Lead", 85000, 4),
+       ("Salesperson", 65000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES  ("Orella", "Abthorpe", 1),
---
+++ utils/questions.js
@@ -0,0 +1,117 @@
+const mainQuestionsMenu = [
+  {
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      {
+        value: "viewAllEmp"
+        name: "View all employees."
+      },
+      {
+        value: "addEmp"
+        name: "Add an employee."
+      },
+      {
+        value: "updateEmpRole"
+        name: "Update employee role."
+      },
+      {
+        value: "viewAllRoles"
+        name: "View all roles."
+      },
+      {
+        value: "addRole"
+        name: "Add a role."
+      },
+      {
+        value: "viewAllDept"
+        name: "View all departments."
+      },
+      {
+        value: "viewEmpByDept"
+        name: "View employees sorted by departments."
+      },
+      {
+        value: "addDept"
+        name: "Add a new department."
+      },
+      {
+        value: "quit"
+        name: "Quit."
+      },
+    ],
+  },
+];
+
+const addNewEmp = [
+  {
+    type: "input",
+    name: "newEmpFirstName",
+    message: "Please type your new employee's first name:",
+  },
+  {
+    type: "input",
+    name: "newEmpLastName",
+    message: "Please type the new employee's last name:",
+  },
+  {
+    type: "list",
+    name: "newEmpRole",
+    message: "What is your new employee's role?",
+    //!how to update?
+    choices: ["HR Director", "Director Finance", "Accountant", "Director Legal", "Lawyer", "Sales Lead", "Salesperson"]
+  },
+];
+
+const updateEmpRole = [
+  {
+    type: "list",
+    name: "updateEmpRoleNames",
+    message: "Which employee's role would you like to update?",
+    //!how to update?
+    choices: ["Orella Abthorpe", "Immanuel Ozanne", "Kimmy Burdus", "Catarina Waddilove", "Elihu Abbotson", "Frederico Guillou", "Phillie Puttnam", "Werner Riccardo"]
+  },
+  {
+    type: "list",
+    name: "updateEmpRoleSelect",
+    message: "Which role to you want to assign to the selected employee?",
+    //!how to update?
+    choices: ["HR Director", "Director Finance", "Accountant", "Director Legal", "Lawyer", "Sales Lead", "Salesperson"]
+  }
+];
+
+const addNewRole = [
+  {
+    type: "input",
+    name: "newRoleName",
+    message: "What is the name of the new role?"
+  },
+  {
+    type: "input",
+    name: "newRoleSalary",
+    message: "What is the salary of the new role?"
+  },
+  {
+    type: "list",
+    name: "newRoleDept",
+    message: "What department does the new role belong to?"
+    //!how to update?
+    choices: ["Human Resources", "Accounting/Finance", "Legal", "Sales"]
+  },
+];
+
+const addNewDept = [
+  {
+    type: "input",
+    name: "newDept",
+    message: "What is the name of the new department?"
+  }
+];
+
+module.exports = {
+  mainQuestionsMenu,
+  addNewEmp,
+  updateEmpRole,
+  addNewRole,
+  addNewDept
+};
