ce28a6e7694d6a33f9d900e44dff55ddc0e726aa
nirmeet-baweja <nirmeet.baweja@gmail.com>
2022-09-01 21:17:16 +0100

    added addNewRole function definition

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ const init = () => {
 
 function viewAllEmp() {
   db.query(
-    `SELECT 
-    employee.employee_id, 
-    employee.first_name, 
-    employee.last_name, 
-    emp_role.title, 
-    department.dept_name, 
-    emp_role.salary, 
+    `SELECT
+    employee.employee_id,
+    employee.first_name,
+    employee.last_name,
+    emp_role.title,
+    department.dept_name,
+    emp_role.salary,
     CONCAT(manager.first_name, " ", manager.last_name) AS manager
   FROM employee
   LEFT JOIN employee AS manager ON employee.manager_id = manager.employee_id
@@ -79,53 +79,100 @@ function viewAllDept() {
 }
 
 function addNewRole() {
-  db.query("SELECT dept_name, department_id FROM department").then(
-    (results) => {
-      console.log(results);
-      const departments = results.map((result) => ({
-        name: result["dept_name"],
-        value: result["department_id"],
-      }));
-      console.log(departments);
+  db.query(
+    "SELECT dept_name AS name, department_id AS value FROM department",
+    (err, departmentChoices) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      console.log(departmentChoices);
       inquirer
         .prompt([
           {
             type: "input",
-            name: "newRoleName",
-            message: "What is the name of the new role?",
+            name: "title",
+            message: "What is the title of the new role?",
           },
           {
             type: "input",
-            name: "newRoleSalary",
+            name: "salary",
             message: "What is the salary of the new role?",
           },
           {
             type: "list",
-            name: "newRoleDept",
+            name: "department_id",
             message: "What department does the new role belong to?",
-            choices: departments,
+            choices: departmentChoices,
           },
         ])
         .then((answers) => {
           console.log(answers);
           db.query(
             "INSERT INTO emp_role (title, salary, department_id) VALUES (?, ?, ?)",
-            [answers.newRoleName, answers.newRoleSalary, answers.newRoleDept],
-            (err, res) => {
-              console.log(res);
+            [answers.title, answers.salary, answers.department_id],
+            (err, result) => {
+              console.log("Im here");
               if (err) {
-                console.log("An error ocurred. Please try again later.");
+                console.log(err);
                 return;
-              } else {
-                console.log("The role has been added to the data base.");
               }
+              console.log(result);
+              init();
             }
           );
-          console.log(`Added ${answers.newRoleName} to the database`);
-          init();
         });
     }
   );
+
+  // db.query("SELECT dept_name, department_id FROM department").then(
+  //   (results) => {
+  //     console.log(results);
+  //     const departments = results.map((result) => ({
+  //       name: result["dept_name"],
+  //       value: result["department_id"],
+  //     }));
+  //     console.log(departments);
+  //     inquirer
+  //       .prompt([
+  //         {
+  //           type: "input",
+  //           name: "newRoleName",
+  //           message: "What is the name of the new role?",
+  //         },
+  //         {
+  //           type: "input",
+  //           name: "newRoleSalary",
+  //           message: "What is the salary of the new role?",
+  //         },
+  //         {
+  //           type: "list",
+  //           name: "newRoleDept",
+  //           message: "What department does the new role belong to?",
+  //           choices: departments,
+  //         },
+  //       ])
+  //       .then((answers) => {
+  //         console.log(answers);
+  //         db.query(
+  //           "INSERT INTO emp_role (title, salary, department_id) VALUES (?, ?, ?)",
+  //           [answers.newRoleName, answers.newRoleSalary, answers.newRoleDept],
+  //           (err, res) => {
+  //             console.log(res);
+  //             if (err) {
+  //               console.log("An error ocurred. Please try again later.");
+  //               return;
+  //             } else {
+  //               console.log("The role has been added to the data base.");
+  //             }
+  //           }
+  //         );
+  //         console.log(`Added ${answers.newRoleName} to the database`);
+  //         init();
+  //       });
+  //   }
+  // );
 }
 
 function addDepartment() {
